a27ec37bbc3be1b8760f7876975b7f7f9ecc1d38
Michael Lafreniere <temike1@gmail.com>
2019-12-05 21:57:07 -0800

    Added user authenication, non-validated user creation and route middleware authenication using JWT

--- server.js
+++ server.js
@@ -5,10 +5,35 @@ const mysql = require('mysql2');
 const cors = require('cors');
 const Filter = require('bad-words');
 const rateLimit = require('express-rate-limit');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
 const filter = new Filter();
+
+let users = [
+  {
+    username: 'Mike',
+    title: 'Post 1'
+  },
+  {
+    username: 'Steve',
+    title: 'Post 2'
+  }
+];
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token === null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 const {
   NODE_ENV,
@@ -17,7 +42,11 @@ const {
   SQL_SERVER,
   SQL_DATABASE,
   SQL_USER,
-  SQL_PASSWORD
+  SQL_PASSWORD,
+  RATE_MSG_PER,
+  RATE_TIME_BETWEEN_MSGS,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET
 } = process.env;
 
 // Verify we have all the data from the .env file we need:
@@ -30,8 +59,16 @@ assert(SQL_USER, 'SQL_USER value not found in .env file.');
 assert(SQL_PASSWORD, 'SQL_PASSWORD value not found in .env file.');
 assert(RATE_MSG_PER, 'RATE_MSG_PER value not found in .env file.');
 assert(
-  RATE_TIME_BETWEEN_MSG,
+  RATE_TIME_BETWEEN_MSGS,
   'RATE_TIME_BETWEEN_MSG value not found in .env file.'
+);
+assert(
+  ACCESS_TOKEN_SECRET,
+  'ACCESS_TOKEN_SECRET value not found in .env file.'
+);
+assert(
+  REFRESH_TOKEN_SECRET,
+  'REFRESH_TOKEN_SECRET value not found in .env file.'
 );
 
 //
@@ -41,7 +78,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname + '/public/views'));
 app.set('port', PORT);
 app.use(cors());
-//app.use(express.static(path.join(__dirname + '/public')));
 app.use(express.json());
 
 //
@@ -66,11 +102,15 @@ app.get('/', (req, res) => {
   res.render('index', {});
 });
 
-app.get('/chirp', (req, res) => {
+app.get('/chirp', authenticateToken, (req, res) => {
   const query = 'SELECT * FROM chirp LIMIT 25';
   connection.query(query, (err, results) => {
     res.json(results);
   });
+});
+
+app.get('/users', (req, res) => {
+  res.send(users);
 });
 
 //
@@ -91,9 +131,9 @@ const isValidChirp = data => {
 //
 // Rate limit posts:
 //
-app.use(
-  rateLimit({ windowMs: RATE_TIME_BETWEEN_MSG * 1000, max: RATE_MSG_PER })
-);
+// app.use(
+//   rateLimit({ windowMs: RATE_TIME_BETWEEN_MSGS * 1000, max: RATE_MSG_PER })
+// );
 
 app.post('/chirp', (req, res) => {
   if (isValidChirp(req.body)) {
@@ -118,6 +158,39 @@ app.post('/chirp', (req, res) => {
     res.status(200).json(chirp);
   } else {
     res.status(422).send({ message: 'Requires a name and a message.' });
+  }
+});
+
+app.post('/create-user', async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPass };
+    users.push(user);
+    req.setTimeout(0);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post('/user-login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user === null) {
+    res.status(400).send('Invalid username/password.');
+  }
+
+  try {
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (result) {
+        const user = { name: 'Mike' };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+        res.json({ accessToken });
+      } else {
+        res.status(401).send(`Invalid username/password.`);
+      }
+    });
+  } catch {
+    res.status(500).send('Failed.');
   }
 });
 
