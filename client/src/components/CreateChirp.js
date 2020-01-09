cf494a1cbd7e95ba97b570c1157002bc8253d498
Michael Lafreniere <temike1@gmail.com>
2020-01-09 14:14:37 -0800

    working on using useReducer to 'cleanup' the interface with CreateChirp.js

--- client/src/components/CreateChirp.js
+++ client/src/components/CreateChirp.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './CreateChirp.css';
 
+const initialState = {
+  text: '',
+  placeholder: 'What is on your mind today?'
+};
+
+const createChirpReducer = (state, action) => {
+  switch (action.type) {
+    case 'text':
+      return {
+        ...state,
+        text: action.value
+      };
+    default:
+      return state;
+  }
+};
+
 export default function CreateChirp() {
-  const text = 'What is on your mind today?';
+  const [state, dispatch] = useReducer(createChirpReducer, initialState);
+
+  const { text, placeholder } = state;
+  const maxChirpLength = 25;
+  const maxChirpsPerPost = 2;
+  const totalLength = maxChirpLength * maxChirpsPerPost;
+  let textRemaining = '';
+  let textRemainingClass = 'remaining-yellow';
+  let splitChirps = '';
+
+  if (text.length > maxChirpLength * 0.2)
+    textRemaining = `${text.length}/${maxChirpLength}`;
+  if (text.length > maxChirpLength * 0.9) textRemainingClass = `remaining-red`;
+  if (text.length > maxChirpLength) {
+    let percent = Math.round(text.length / maxChirpLength);
+    splitChirps += ` ${percent}/${maxChirpsPerPost} chirps`;
+  }
+
   return (
     <>
       <div className="create-chirp">
@@ -21,12 +55,22 @@ export default function CreateChirp() {
           <input
             type="text"
             className="my-input"
-            placeholder="What is on your mind today?"
+            value={text}
+            onChange={event =>
+              dispatch({ type: 'text', value: event.currentTarget.value })
+            }
+            placeholder={placeholder}
+            maxLength={totalLength}
           />
-          {/* <div className="input-div" contentEditable="true" spellCheck="true">
-            <span>{text}</span>
-            <span className="over-typed"></span>
-          </div> */}
+          <div className="add-images">
+            <button>
+              <span>+</span>
+            </button>
+          </div>
+          <div className="remaining">
+            <span className={textRemainingClass}>{textRemaining}</span>
+            <span className="split-chirps">{splitChirps}</span>
+          </div>
         </div>
       </div>
     </>
